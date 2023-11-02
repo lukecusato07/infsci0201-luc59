990800b0228be15ce689b76f6d9dbf46e0e57997
lukecusato07 <lukecusato1@gmail.com>
2023-11-02 14:32:12 -0400

    adding json settings

--- flask_app/.venv/app.py
+++ flask_app/.venv/app.py
@@ -7,5 +7,4 @@ def hello_word():
     return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 8000, debug = True)
-
+    app.run(host = "0.0.0.0", port = 8000, debug = True) 
