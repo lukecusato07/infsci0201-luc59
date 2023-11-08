fafc8275f0bdc16ed71808a129c7ea1962225c77
lukecusato07 <lukecusato1@gmail.com>
2023-11-08 12:21:18 -0500

    modifying flask app

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World!</p><h1>This is my Flask app</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
