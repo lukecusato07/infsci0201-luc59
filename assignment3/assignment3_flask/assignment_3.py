e85d14a05c3fcd9daeef71a2fb651eb7118f1472
lukecusato07 <lukecusato1@gmail.com>
2023-12-07 13:04:54 -0500

    part #2 progress

--- assignment3/assignment3_flask/assignment_3.py
+++ assignment3/assignment3_flask/assignment_3.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import json
 
 from model.smart_devices import Home
 from model.smart_devices import SmartDevice
@@ -14,6 +15,31 @@ def index():
     home.add_device(bulb)
 
     return render_template("home.html", home=home)
+
+@app.route('/add_lightbulb', methods=['POST'])
+def add_lightbulb():
+    data = request.json
+    try:
+        name = data['name']
+        manufacturer = data['manufacturer']
+        brightness = data['brightness']
+
+        new_bulb = LightBulb(name, manufacturer, brightness)
+
+        light_bulb_to_json = new_bulb.to_json()
+
+        with open('light.json', 'w') as file:
+            file.write(light_bulb_to_json)
+            #json.dump(new_bulb.to_json(), file)
+            
+        return jsonify({"message": "LightBulb added successfully"}), 200
+
+    except KeyError:
+        return jsonify({"error": "Missing data for creating a LightBulb"}), 400
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
