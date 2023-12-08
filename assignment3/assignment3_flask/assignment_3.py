eadfc1cd3051de638314c7011c3050292cba8093
lukecusato07 <lukecusato1@gmail.com>
2023-12-08 12:44:24 -0500

    fixing part #2 json

--- assignment3/assignment3_flask/assignment_3.py
+++ assignment3/assignment3_flask/assignment_3.py
@@ -1,7 +1,6 @@
 # Part #2: Create a Flask App for Home Devices in Part#1
 from flask import Flask, render_template, request, jsonify
 import json
-import os 
 
 from model.smart_devices import Home
 from model.smart_devices import SmartDevice
@@ -15,7 +14,6 @@ def index():
     bulb2 = LightBulb("SmartBulb2", "Philips", 75.5)
 
     home2.add_device(bulb2)
-
     return render_template("home.html", home=home2)
 
 @app.route('/add_lightbulb', methods=['POST'])
@@ -29,16 +27,11 @@ def add_lightbulb():
         new_bulb = LightBulb(name, manufacturer, brightness)
         light_bulb_json = new_bulb.to_json()
 
-        light_json_path = 'light.json'
-        if not os.path.exists(light_json_path):
-            with open(light_json_path, 'w') as file:
-                json.dump([], file)
+        light_path = 'light.json'
+        
+        with open(light_path, 'w') as file:
+            json.dump(light_bulb_json, file, indent=4)
 
-        with open(light_json_path, 'r+') as file:
-            file_data = json.load(file)
-            file_data.append(json.loads(light_bulb_json))
-            file.seek(0)
-            json.dump(file_data, file, indent=4)
 
         return jsonify({"message": "LightBulb added successfully"}), 200
 
