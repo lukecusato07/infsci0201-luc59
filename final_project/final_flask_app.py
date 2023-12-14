7ba46b7cb55ec9e762910ce18b66bbbddffc1d24
lukecusato07 <lukecusato1@gmail.com>
2023-12-14 16:36:58 -0500

    progress

--- final_project/final_flask_app.py
+++ final_project/final_flask_app.py
@@ -1,20 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 import json
 import requests
-
 from devices import LightBulb, Vacuum, Thermostat, Home, NETWORK_CONNECTION_MESSAGE
 
 WEATHER_API_KEY = "a3fJhWTXjyT0t8xP649BXVzaWEIdX5Rq"
+FILE_PATH = 'data/devices.json'
 
+devices = []
 app = Flask(__name__)
 
-def load_devices():
-    try:
-        with open('data/devices.json', 'r') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return []
-    
 
 def save_devices(devices):
     with open('data/devices.json', 'w') as file:
@@ -22,30 +16,57 @@ def save_devices(devices):
 
 
 @app.route('/')
-def home():
-    devices = load_devices()
-    return render_template('home.html', devices=devices)
+def index():
+    bulb2 = LightBulb("SmartBulb", "Philips", 50.5)
+    vacuum2 = Vacuum("SmartVacuum", "Dyson", 100)
+    thermostat2 = Thermostat("SmartThermostat", "Nest", "Master Bedroom", 72)
+    home2 = Home("135 N Bellfield Ave", 15213)
+    
+    home2.add_device(bulb2)
+    home2.add_device(vacuum2)
+    home2.add_device(thermostat2)
+
+    return render_template('home.html', home=home2)
 
 
 @app.route('/devices', methods=['GET'])
 def get_devices():
-    devices = load_devices()
+    devices = index()
     return jsonify(devices)
 
 
 @app.route('/devices', methods=['POST'])
 def add_device():
     data = request.json
-    devices = load_devices()
-    devices.append(data)
-    save_devices(devices)
-    return jsonify({'message': 'Device added successfully'})
+
+    try:
+        name = data['name']
+        manufacturer = data['manufacturer']
+        brightness = data['brightness']
+        battery = data['battery']
+        room = data['room']
+        temp = data['temperature']
+
+        new_thermostat = Thermostat(name, manufacturer, room, temp)
+        thermostat_to_json = new_thermostat.to_json()
+        
+        with open(FILE_PATH, 'w') as file:
+            json.dump(thermostat_to_json, file, indent=4)
+
+
+        return jsonify({'message': 'Termostat added successfully.'}), 200
+    
+    except KeyError:
+        return jsonify({'error': 'Missing data for creating a thermostat'}), 400
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 @app.route('/devices/<int:device_id>', methods=['PUT'])
 def update_device(device_id):
     data = request.json
-    devices = load_devices()
+    
     if device_id < 0 or device_id >= len(devices):
         return jsonify({'error': 'Device not found'}), 404
     devices[device_id] = data
@@ -55,13 +76,14 @@ def update_device(device_id):
 
 @app.route('/devices/<int:device_id>', methods=['DELETE'])
 def delete_device(device_id):
-    devices = load_devices()
+    
     if device_id < 0 or device_id >= len(devices):
         return jsonify({'error': 'Device not found'}), 404
     
     del devices[device_id]
     save_devices(devices)
     return jsonify({'message': 'Device deleted successfully'})
+
 
 @app.route('/temperature_check', methods=['GET'])
 def temp_check():
@@ -78,7 +100,6 @@ def temp_check():
     else:
         return jsonify({'error': 'Failed to fetch external temperature'}), 500
     
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
